3972dc51ccd40c80305384120560b8f10ccb3104
brpradeepprabhu <pbellikoth001@nam.ad.pwcinternal.com>
2017-08-18 18:41:30 +0530

    grid

---
+++ assets/scripts/appgrid/appgrid.component.spec.ts
@@ -0,0 +1,25 @@
+﻿import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AppGridComponent } from './appgrid.component';
+
+describe('AppGridComponent', () => {
+  let component: AppGridComponent;
+  let fixture: ComponentFixture<AppGridComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AppGridComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppGridComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should be created', () => {
+    expect(component).toBeTruthy();
+  });
+});
